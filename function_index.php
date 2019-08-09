c097351149ac8b1a22618b256789f783fa9f6618
ADMIN <ADMIN@LAPTOP-V9QMD0N7>
2019-08-09 15:52:55 +0700

    thaydoi.index

---
+++ function_index.php
@@ -0,0 +1,16 @@
+<?php	function product_shoes(){	
+		$db = getDB();// Connect to database
+		$query ="SELECT * FROM  products
+		 LIMIT 8";
+		try {
+			$statement = $db->prepare($query);
+			$statement->execute();
+			$result = $statement->fetchAll();
+			$statement->closeCursor();
+			return $result;
+		} catch (PDOException $e) {
+			$error_message = $e->getMessage();
+			echo "Error execute query statement:".$error_message; 
+		}
+	}
+?>
